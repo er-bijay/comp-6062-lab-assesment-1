78813453870a632db882f5747a90d3215986df33
er-bijay <149816466+er-bijay@users.noreply.github.com>
2024-06-12 20:28:16 -0400

    hh

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+const myFullName = "Bijay Gurung";
+const myStudentNUmber = "1171265";
+
+const result = `{myFullName}-${myStudentNUmber}`;
+console.log(result);
+
+const headerContent = document.querySelector('h1');
+headerContent.innerHTML = result;
+headerContent.classList.add('headingPrimary');
